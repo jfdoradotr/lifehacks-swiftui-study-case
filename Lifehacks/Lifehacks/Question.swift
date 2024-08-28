6e4e74b19243517eca2da5bd13f4de1ae33d346b
Juan Dorado <jfdoradotr@icloud.com>
2024-08-27 20:05:30 -0600

    Create the small logic to handle the votation

--- Lifehacks/Lifehacks/Question.swift
+++ Lifehacks/Lifehacks/Question.swift
@@ -10,13 +10,35 @@ import Foundation
 struct Question {
   let isAnswered: Bool
   let id: Int
-  let score: Int
   let viewCount: Int
   let answerCount: Int
   let title: String
   let body: String
   let creationDate: Date
   let owner: User?
+
+  private(set) var score: Int
+  private(set) var vote: Vote?
+
+  mutating func unvote() {
+    guard let vote else { return }
+    score -= vote.rawValue
+    self.vote = nil
+  }
+
+  mutating func upvote() {
+    guard vote != .up else { return }
+    unvote()
+    score += Vote.up.rawValue
+    vote = .up
+  }
+
+  mutating func downvote() {
+    guard vote != .down else { return }
+    unvote()
+    score += Vote.down.rawValue
+    vote = .down
+  }
 }
 
 extension Question {
