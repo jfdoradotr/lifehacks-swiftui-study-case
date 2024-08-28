ac2ff790a2b75f23a168a302556e56e8b527a713
Juan Dorado <jfdoradotr@icloud.com>
2024-08-27 19:45:14 -0600

    Create base project

---
+++ Lifehacks/Lifehacks/LifehacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifehacksApp.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LifehacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
