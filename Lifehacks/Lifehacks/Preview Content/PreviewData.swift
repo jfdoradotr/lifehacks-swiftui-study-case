cbf934882c917baeccf9e353053c4bf417c067cb
Juan Dorado <jfdoradotr@icloud.com>
2024-08-28 11:43:00 -0600

    Replace the preview data to use the `[Question].preview` values

--- Lifehacks/Lifehacks/Preview Content/PreviewData.swift
+++ Lifehacks/Lifehacks/Preview Content/PreviewData.swift
@@ -11,13 +11,7 @@ import Foundation
 
 extension User {
   static var preview: User {
-    User(
-      id: 0,
-      reputation: 1_234,
-      name: "Betty Vazquez",
-      aboutMe: "The monkey-rope is found in all whalers; but it was only in the Pequod that the monkey and his holder were ever tied together. This improvement upon the original usage was introduced by no less a man than Stubb, in order to afford the imperilled harpooneer the strongest possible guarantee for the faithfulness and vigilance of his monkey-rope holder.",
-      profileImageURL: nil
-    )
+    Question.preview.owner!
   }
 }
 
@@ -25,17 +19,7 @@ extension User {
 
 extension Question {
   static var preview: Question {
-    Question(
-      isAnswered: true,
-      id: 0,
-      viewCount: 2_770,
-      answerCount: 9,
-      title: "In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew.",
-      body: "So strongly and metaphysically did I conceive of my situation then, that while earnestly watching his motions. I seemed distinctly to perceive that my own individuality was now merged in a joint stock company of two; that my free will had received a mortal wound; and that another's mistake or misfortune might plunge innocent me into unmerited disaster and death.",
-      creationDate: .init(),
-      owner: .preview,
-      score: 359
-    )
+    [Question].preview[0]
   }
 }
 
