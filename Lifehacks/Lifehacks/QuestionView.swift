753575e61927706c70699b2bcdad11d4d5f55435
Juan Dorado <jfdoradotr@icloud.com>
2024-08-28 10:17:25 -0600

    Create the file for `Question` view

---
+++ Lifehacks/Lifehacks/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 28/08/24.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+  var body: some View {
+    Text("In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among the crew.")
+  }
+}
+
+#Preview {
+  QuestionView()
+}
