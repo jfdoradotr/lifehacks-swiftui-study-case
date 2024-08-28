de55f639c2366399066b7b5d88ec92f0b6a0ab83
Juan Dorado <jfdoradotr@icloud.com>
2024-08-27 19:49:37 -0600

    Create the `User` model

---
+++ Lifehacks/Lifehacks/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 27/08/24.
+//
+
+import Foundation
+
+struct User {
+  let id: Int
+  let reputation: Int
+  let name: String
+  let aboutMe: String?
+  let profileImageURL: URL?
+}
