698af1d628c1725855225a67b4fdcc4b55915a5e
Juan Dorado <jfdoradotr@icloud.com>
2024-08-30 13:50:34 -0600

    Improve with spacing and alignment

--- Lifehacks/Lifehacks/QuestionView.swift
+++ Lifehacks/Lifehacks/QuestionView.swift
@@ -11,7 +11,7 @@ struct QuestionView: View {
   let question: Question
 
   var body: some View {
-    VStack {
+    VStack(alignment: .leading, spacing: 8.0) {
       Text(question.title)
         .font(.headline)
       Text("Asked on Sep 15th, 2019")
