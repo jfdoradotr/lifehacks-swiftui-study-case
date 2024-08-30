3ce0457515a1218ec9b6befabbbadbf00c3751c2
Juan Dorado <jfdoradotr@icloud.com>
2024-08-30 13:55:18 -0600

    Group in a `HStack` to add the vote buttons (this is a preparation step)

--- Lifehacks/Lifehacks/QuestionView.swift
+++ Lifehacks/Lifehacks/QuestionView.swift
@@ -11,15 +11,17 @@ struct QuestionView: View {
   let question: Question
 
   var body: some View {
-    VStack(alignment: .leading, spacing: 8.0) {
-      Text(question.title)
-        .font(.headline)
-      Group {
-        Text("Asked on \(question.creationDate.formatted(date: .long, time: .omitted))")
-        Text("Viewed \(question.viewCount.formatted()) times")
+    HStack(alignment: .top, spacing: 16.0) {
+      VStack(alignment: .leading, spacing: 8.0) {
+        Text(question.title)
+          .font(.headline)
+        Group {
+          Text("Asked on \(question.creationDate.formatted(date: .long, time: .omitted))")
+          Text("Viewed \(question.viewCount.formatted()) times")
+        }
+        .font(.caption)
+        .foregroundStyle(.secondary)
       }
-      .font(.caption)
-      .foregroundStyle(.secondary)
     }
   }
 }
