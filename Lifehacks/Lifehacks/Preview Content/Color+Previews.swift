8fb2d43c014ad44d88469adacab65e69a3fc9c2a
Juan Dorado <jfdoradotr@icloud.com>
2024-09-02 11:55:47 -0600

    Create the color accets and a Color Preview file

---
+++ Lifehacks/Lifehacks/Preview Content/Color+Previews.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Previews.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 02/09/24.
+//
+
+import SwiftUI
+
+#Preview {
+  VStack {
+    Color.accentColor
+    Color.pizazz
+    Color.electricViolet
+    Color.blazeOrange
+  }
+}
