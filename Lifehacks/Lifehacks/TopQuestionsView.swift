0a669de5d3536949c79dd4a2050856cb23bb0501
Juan Dorado <jfdoradotr@icloud.com>
2024-09-03 20:46:17 -0600

    Create the Counter subview of Row

--- Lifehacks/Lifehacks/TopQuestionsView.swift
+++ Lifehacks/Lifehacks/TopQuestionsView.swift
@@ -13,6 +13,36 @@ struct TopQuestionsView: View {
   }
 }
 
+// MARK: - Row
+
+private extension TopQuestionsView {
+  struct Row: View {
+    var body: some View {
+      Text("Hello, World!")
+    }
+  }
+}
+
+// MARK: - Counter
+
+private extension TopQuestionsView.Row {
+  struct Counter: View {
+    let count: Int
+    let label: String
+
+    var body: some View {
+      VStack {
+        Text("\(count)")
+          .font(.title3)
+          .bold()
+        Text(label)
+          .font(.caption)
+      }
+      .frame(width: 67, height: 67)
+    }
+  }
+}
+
 // MARK: - Previews
 
 #Preview {
