21e8305f5b98e32795a38fd3c44e12ad168d5d74
Juan Dorado <jfdoradotr@icloud.com>
2024-09-03 20:43:58 -0600

    Create the top questions view file

---
+++ Lifehacks/Lifehacks/TopQuestionsView.swift
@@ -0,0 +1,20 @@
+//
+//  TopQuestionsView.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 03/09/24.
+//
+
+import SwiftUI
+
+struct TopQuestionsView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+// MARK: - Previews
+
+#Preview {
+  TopQuestionsView()
+}
