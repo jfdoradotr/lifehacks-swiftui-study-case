2ca5fcdf7ab794c814974c341ee7b59dac7c8a34
Juan Dorado <jfdoradotr@icloud.com>
2024-09-06 14:58:41 -0600

    Organize modular views in separate files

---
+++ Lifehacks/Lifehacks/QuestionView.Voting.swift
@@ -0,0 +1,99 @@
+//
+//  QuestionView.Voting.swift
+//  Lifehacks
+//
+//  Created by Juan Francisco Dorado Torres on 06/09/24.
+//
+
+import SwiftUI
+
+extension QuestionView {
+  struct Voting: View {
+    let score: Int
+    let vote: Vote?
+    let upvote: () -> Void
+    let downvote: () -> Void
+    let unvote: () -> Void
+
+    enum Vote {
+      case up, down
+    }
+
+    var body: some View {
+      VStack(spacing: 8.0) {
+        VoteButton(
+          buttonType: .up,
+          highlighted: vote == .up,
+          action: { cast(vote: .up) }
+        )
+        Text("\(score)")
+          .font(.title)
+          .foregroundStyle(.secondary)
+        VoteButton(
+          buttonType: .down,
+          highlighted: vote == .down,
+          action: { cast(vote: .down) }
+        )
+      }
+    }
+
+    private func cast(vote: Vote) {
+      switch (self.vote, vote) {
+      case (nil, .up), (.down, .up): upvote()
+      case (nil, .down), (.up, .down): downvote()
+      default: unvote()
+      }
+    }
+  }
+}
+
+extension QuestionView.Voting.Vote {
+  init?(vote: Lifehacks.Vote?) {
+    switch vote {
+    case .up: self = .up
+    case .down: self = .down
+    case .none: return nil
+    }
+  }
+}
+
+// MARK: - VoteButton
+
+private extension QuestionView.Voting {
+  struct VoteButton: View {
+    let buttonType: ButtonType
+    let highlighted: Bool
+    let action: () -> Void
+
+    var body: some View {
+      Button(action: action, label: {
+        buttonType.image(highlighted: highlighted)
+          .resizable()
+          .frame(width: 32, height: 32)
+      })
+    }
+  }
+}
+
+private extension QuestionView.Voting.VoteButton {
+  enum ButtonType: String {
+    case up = "arrowtriangle.up"
+    case down = "arrowtriangle.down"
+
+    func image(highlighted: Bool) -> Image {
+      let imageName = rawValue + (highlighted ? ".fill" : "")
+      return Image(systemName: imageName)
+    }
+  }
+}
+
+// MARK: - Previews
+
+#Preview(traits: .sizeThatFitsLayout) {
+  HStack(spacing: 16) {
+    QuestionView.Voting.VoteButton(buttonType: .up, highlighted: true, action: {})
+    QuestionView.Voting.VoteButton(buttonType: .up, highlighted: false, action: {})
+    QuestionView.Voting.VoteButton(buttonType: .down, highlighted: true, action: {})
+    QuestionView.Voting.VoteButton(buttonType: .down, highlighted: false, action: {})
+  }
+}
--- Lifehacks/Lifehacks/QuestionView.swift
+++ Lifehacks/Lifehacks/QuestionView.swift
@@ -63,88 +63,6 @@ private extension QuestionView.Info {
   }
 }
 
-// MARK: - Voting
-
-private extension QuestionView {
-  struct Voting: View {
-    let score: Int
-    let vote: Vote?
-    let upvote: () -> Void
-    let downvote: () -> Void
-    let unvote: () -> Void
-
-    enum Vote {
-      case up, down
-    }
-
-    var body: some View {
-      VStack(spacing: 8.0) {
-        VoteButton(
-          buttonType: .up,
-          highlighted: vote == .up,
-          action: { cast(vote: .up) }
-        )
-        Text("\(score)")
-          .font(.title)
-          .foregroundStyle(.secondary)
-        VoteButton(
-          buttonType: .down,
-          highlighted: vote == .down,
-          action: { cast(vote: .down) }
-        )
-      }
-    }
-
-    private func cast(vote: Vote) {
-      switch (self.vote, vote) {
-      case (nil, .up), (.down, .up): upvote()
-      case (nil, .down), (.up, .down): downvote()
-      default: unvote()
-      }
-    }
-  }
-}
-
-private extension QuestionView.Voting.Vote {
-  init?(vote: Lifehacks.Vote?) {
-    switch vote {
-    case .up: self = .up
-    case .down: self = .down
-    case .none: return nil
-    }
-  }
-}
-
-// MARK: - VoteButton
-
-private extension QuestionView.Voting {
-  struct VoteButton: View {
-    let buttonType: ButtonType
-    let highlighted: Bool
-    let action: () -> Void
-
-    var body: some View {
-      Button(action: action, label: {
-        buttonType.image(highlighted: highlighted)
-          .resizable()
-          .frame(width: 32, height: 32)
-      })
-    }
-  }
-}
-
-private extension QuestionView.Voting.VoteButton {
-  enum ButtonType: String {
-    case up = "arrowtriangle.up"
-    case down = "arrowtriangle.down"
-
-    func image(highlighted: Bool) -> Image {
-      let imageName = rawValue + (highlighted ? ".fill" : "")
-      return Image(systemName: imageName)
-    }
-  }
-}
-
 // MARK: - QuestionBody
 
 private extension QuestionView {
@@ -204,15 +122,6 @@ private extension QuestionView.Owner {
 
 #Preview {
   QuestionView(question: .preview)
-}
-
-#Preview("Vote Button", traits: .sizeThatFitsLayout) {
-  HStack(spacing: 16) {
-    QuestionView.Voting.VoteButton(buttonType: .up, highlighted: true, action: {})
-    QuestionView.Voting.VoteButton(buttonType: .up, highlighted: false, action: {})
-    QuestionView.Voting.VoteButton(buttonType: .down, highlighted: true, action: {})
-    QuestionView.Voting.VoteButton(buttonType: .down, highlighted: false, action: {})
-  }
 }
 
 #Preview("Accessibility", traits: .fixedLayout(width: 320, height: 568)) {
