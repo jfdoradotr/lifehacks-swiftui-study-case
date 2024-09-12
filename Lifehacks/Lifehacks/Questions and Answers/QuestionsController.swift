4aa0cdfc2424da9c42d74dcd1b64f4bcb9791459
Juan Dorado <jfdoradotr@icloud.com>
2024-09-12 14:21:08 -0600

    Due we already takes a questions as a parameter on question view we just need to locate the corresponding index in the array, so we just need a way to locate the question of the array and QuestionsController already has that array

--- Lifehacks/Lifehacks/Questions and Answers/QuestionsController.swift
+++ Lifehacks/Lifehacks/Questions and Answers/QuestionsController.swift
@@ -12,8 +12,19 @@ final class QuestionsController: ObservableObject {
 
   private let persistenceController: PersistenceController
 
+  subscript(id: Int) -> Question {
+    get { questions[index(for: id)] }
+    set { questions[index(for: id)] = newValue }
+  }
+
   init(persistenceController: PersistenceController) {
     self.questions = persistenceController.fetchQuestions() ?? []
     self.persistenceController = persistenceController
+  }
+}
+
+private extension QuestionsController {
+  func index(for id: Int) -> Int {
+    questions.firstIndex(where: { $0.id == id })!
   }
 }
