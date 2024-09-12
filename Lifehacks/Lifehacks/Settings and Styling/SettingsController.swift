541d3196a905ba9b21e2c703086c15baff27ef98
Juan Dorado <jfdoradotr@icloud.com>
2024-09-12 13:49:26 -0600

    Store the selected settings by the user on the user defaults

--- Lifehacks/Lifehacks/Settings and Styling/SettingsController.swift
+++ Lifehacks/Lifehacks/Settings and Styling/SettingsController.swift
@@ -8,12 +8,28 @@
 import Foundation
 
 final class SettingsController: ObservableObject {
-  @Published var selectedThemeID: String? = Theme.default.id
+  @Published var selectedThemeID: String? = Theme.default.id {
+    didSet {
+      defaults.set(selectedThemeID, forKey: UserDefaultKeys.themeID)
+    }
+  }
+
+  private var defaults = UserDefaults.standard
 
   var theme: Theme {
     guard let selectedThemeID, let theme = Theme.allThemes.first(where: { $0.id == selectedThemeID }) else {
       return Theme.default
     }
     return theme
+  }
+
+  init() {
+    selectedThemeID = defaults.string(forKey: UserDefaultKeys.themeID)
+  }
+}
+
+private extension SettingsController {
+  struct UserDefaultKeys {
+    static let themeID = "ThemeID"
   }
 }
